91f10d12b9f1748eb223efa1c84e3878128982a6
Gaurav kumar <gk6760788@gmail.com>
2026-03-16 13:09:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let hour=new Date().getHours()
-
-if(hour>6 && hour<18){
-document.body.style.background="linear-gradient(#87ceeb,#1e90ff)"
-}else{
-document.body.style.background="linear-gradient(#0f2027,#203a43,#2c5364)"
-}
